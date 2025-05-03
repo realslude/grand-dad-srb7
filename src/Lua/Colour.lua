5a1dc8b1a774e1fc2da77efca8491f0a5b62d808
Sludesus <98156333+realslude@users.noreply.github.com>
2025-05-03 23:27:36 +0200

    initial commit

---
+++ src/Lua/Colour.lua
@@ -0,0 +1,9 @@
+freeslot("SKINCOLOR_SEVEN")
+skincolors[SKINCOLOR_SEVEN] = {
+	name = "Seven",
+	ramp = {32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32},
+	invcolor = SKINCOLOR_GOLD,
+	invshade = 0,
+	chatcolor = V_REDMAP,
+	accessible = false
+}
