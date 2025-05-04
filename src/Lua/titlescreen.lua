6d7731fcceee49b5ba78287bbd8f8c1ffd10671f
Sludesus <98156333+realslude@users.noreply.github.com>
2025-05-04 15:47:33 +0200

    tweaks and additions

---
+++ src/Lua/titlescreen.lua
@@ -0,0 +1,10 @@
+-- reusing stuff from srb5b yay
+/* addHook("HUD", function(v)
+	v.drawScaled(FU, 15*FU, FU, v.cachePatch("ONENINENINETWO"), V_SNAPTOBOTTOM)
+end, "copyright")
+
+addHook("HUD", function(v)
+	v.drawScaled(FU, FU, 4*FU, v.cachePatch("GRANDDAD"))
+end, "title")
+*/
+-- nvm doesnt work
